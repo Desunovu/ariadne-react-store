de78a8c5cde75467fbe11058e8a278ac48bdfa7a
desunovu <desubovu@gmail.com>
2023-03-26 15:48:55 +0300

    sync commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
+import Login from "./pages/login";
+import Register from "./pages/register";
 import Navbar from "./components/navbar";
 
 function App() {
@@ -10,8 +12,8 @@ function App() {
         <Navbar/>
         <Routes>
             <Route path="/" element={<Homepage/>}/>
-            <Route path="/login" />
-            <Route path="/register" />
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
         </Routes>
     </div>
   );
---
+++ src/pages/register.js
@@ -0,0 +1,77 @@
+import React from "react";
+import {useContext, useState} from "react";
+import {AuthContext} from "../context/authContext";
+import {useForm} from "../utility/hooks";
+import {useMutation, gql} from "@apollo/react-hooks";
+import {TextField, Button, Container, Stack, Alert} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+
+const CREATE_USER = gql`
+    mutation CreateUser(
+        $email: String!
+        $password: String!
+    ) {
+        createUser(email: $email, password: $password){
+            status
+            errors{message}
+            user{email}
+        }
+    }
+`;
+
+function Register(props) {
+    const context = useContext(AuthContext);
+    let navigate = useNavigate();
+    const [errors, setErrors] = useState([]);
+
+    function createUserCallback() {
+        console.log("Callback hit");
+        createUser();
+    }
+
+    const { onChange, onSubmit, values } = useForm(createUserCallback, {
+        email: "",
+        password: ""
+    })
+
+    const [createUser, { loading, error, data }] = useMutation(
+        CREATE_USER, {
+            onError({ graphQLErrors}) {
+                setErrors(graphQLErrors);
+            },
+            variables: {
+                email: values.email,
+                password: values.password
+            }
+        }
+    );
+
+    return(
+        <Container spacing={2} maxWidth="sm">
+            <h3>Register</h3>
+            <p>Всем привет</p>
+            <Stack spacing={2} paddingBottom={2}>
+                <TextField
+                    label="E-mail"
+                    name="email"
+                    onChange={onChange}
+                />
+                 <TextField
+                    label="Пароль"
+                    name="password"
+                    onChange={onChange}
+                />
+            </Stack>
+            {errors.map(function(error){
+                return(
+                    <Alert severity="error">
+                        {error.message}
+                    </Alert>
+                )
+            })}S
+            <Button variant="contained" onClick={onSubmit}>Создать учетную запись</Button>
+        </Container>
+    )
+}
+
+export default Register;
