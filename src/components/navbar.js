cd6956090bd9e0d63c642b686f70994e2485d341
Kirill Tatarinov <Desunovu@gmail.com>
2023-04-14 14:14:31 +0300

    Изменение расположения кнопки админ-панели

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,6 +2,7 @@ import React, {useContext} from "react";
 import { AppBar, Box, Toolbar, Typography, Button } from "@mui/material";
 import {Link, useNavigate} from "react-router-dom";
 import {AuthContext} from "../context/authContext";
+import StoreTwoToneIcon from '@mui/icons-material/StoreTwoTone';
 
 function Navbar() {
     const { user, logout } = useContext(AuthContext);
@@ -22,17 +23,23 @@ function Navbar() {
                 sx={{ zIndex: (theme) => theme.zIndex.drawer +  1}}
             >
                 <Toolbar>
-                    <Typography variant="h5" component="div">
+                    {/*Заголовок магазина*/}
+                    <Typography variant="h5" component="div" sx={{display: "flex", alignItems: "center"}}>
+                        <StoreTwoToneIcon/>
                         <Link to="/" style={{textDecoration: "none", color: "white"}}>D3$uN0Vu STORE</Link>
                     </Typography>
+                    {/*Кнопка админ-панели*/}
+                    {document.location.pathname !== "/admin" &&
+                        <Typography variant="body" component="div">
+                            <Link to="/admin" style={{textDecoration: "none", color: "white", marginLeft: "20px"}}>
+                                Панель администратора
+                            </Link>
+                        </Typography>
+                    }
+                    {/*Кнопки пользователя, входа, выхода, регистрации*/}
                     <Box alignItems="right" sx={{flexGrow: 1, textAlign: "right"}}>
                         { user ?
                             <div>
-                                {document.location.pathname !== "/admin" &&
-                                    <Link to="/admin" sx={{textDecoration: "none", color: "white", marginRight: "10px"}}>
-                                        Панель администратора
-                                    </Link>
-                                }
                                 <Button style={{textDecoration: "none", color: "white"}} onClick={onLogout}>Выйти</Button>
                             </div>
                             :
