756a2a98a14ae32a59635782c718300c32715502
Kirill Tatarinov <Desunovu@gmail.com>
2023-04-19 22:37:25 +0300

    Исправление обработчика ошибок
    - исправлено отображение graphQLErrors

--- src/components/ErrorsHandler.js
+++ src/components/ErrorsHandler.js
@@ -3,16 +3,19 @@ import {Alert} from "@mui/material";
 
 export default function ErrorsHandler(props){
     const { errors, apolloError } = props;
+    console.log(apolloError);
 
     return (
         <>
             {apolloError &&
-                ((apolloError.networkError &&
-                    <Alert severity="error">Сетевая ошибка: "{apolloError.networkError.message}"</Alert>)
-                ||
-                (apolloError.graphQLErrors &&
-                    <Alert severity="error">{apolloError.graphQLErrors}</Alert>
-                ))
+                (
+                    (apolloError.networkError &&
+                        <Alert severity="error">Сетевая ошибка: "{apolloError.networkError.message}"</Alert>)
+                    ||
+                    (apolloError.graphQLErrors.map(error => (
+                        <Alert severity="error">{error.message}</Alert>
+                    )))
+                )
             }
             {errors.map(error => (
                     <Alert key={error.id} severity="warning">{error.message}</Alert>
