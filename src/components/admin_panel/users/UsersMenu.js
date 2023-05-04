4e351c4fe667b29270ad8318313d9c1d2875ad0f
Kirill Tatarinov <Desunovu@gmail.com>
2023-05-04 14:04:29 +0300

    Исправление UsersMenu после переноса запроса

--- src/components/admin_panel/users/UsersMenu.js
+++ src/components/admin_panel/users/UsersMenu.js
@@ -1,72 +1,29 @@
-import React, {useContext, useState} from "react";
-import {
-    Box,
-    Typography,
-    Button,
-    Drawer,
-    List,
-    ListItem,
-    ListItemText,
-    ListItemButton, Toolbar, Container, Grid
-} from "@mui/material";
-import {Link, useNavigate} from "react-router-dom";
-import {AuthContext} from "../../../context/authContext";
-import {gql, useQuery} from "@apollo/react-hooks";
+import React, { useState } from "react";
+import { Container, Grid } from "@mui/material";
+import { useQuery } from "@apollo/react-hooks";
 import UserElement from "./UserElement";
+import { GET_USERS } from "../../../operations/queries/getUsers";
 
-const GET_USERS=gql`
-    query GetUsers(
-        $pagination: Pagination
-        $sort: SortGetUsers
-    ) {
-        getUsers(
-           pagination: $pagination,
-           sort: $sort
-        ){
-            status
-            errors {
-              code
-              message
-            }
-            users {
-              id
-              email
-              role
-              avatarUrl
-              firstName
-              lastName
-              address
-              phoneNumber
-            }
-        }
-    }
-`
+function UsersMenu() {
+  const [users, setUsers] = useState([]);
 
-function UsersMenu(props) {
-    const { user } = useContext(AuthContext);
-    let navigate = useNavigate();
-    const [page, setPage] = useState(1);
-    const [users, setUsers] = useState([]);
+  useQuery(GET_USERS, {
+    onCompleted: (data) => {
+      console.log(data);
+      setUsers(data.getUsers.users);
+    },
+  });
 
-    const {data} = useQuery(
-        GET_USERS, {
-            onCompleted: (data) => {
-                console.log(data)
-                setUsers(data.getUsers.users)
-            }
-        }
-    )
-
-    return (
-        <Container spacing={2} maxWidth="md">
-            <h3>Список пользователей сайта</h3>
-            <Grid container spacing={5}>
-                {users.map((_user) => (
-                    <UserElement user={_user}/>
-                ))}
-            </Grid>
-        </Container>
-    )
+  return (
+    <Container spacing={2} maxWidth="md">
+      <h3>Список пользователей сайта</h3>
+      <Grid container spacing={5}>
+        {users.map((user) => (
+          <UserElement user={user} />
+        ))}
+      </Grid>
+    </Container>
+  );
 }
 
 export default UsersMenu;
