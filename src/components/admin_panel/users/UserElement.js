ef075f3045435057375de198db13868b41cd1a99
Kirill Tatarinov <Desunovu@gmail.com>
2023-05-04 18:06:01 +0300

    Рефракторинг UserElement

--- src/components/admin_panel/users/UserElement.js
+++ src/components/admin_panel/users/UserElement.js
@@ -1,41 +1,68 @@
-import React from "react";
-import {Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, Item, Typography} from "@mui/material";
+import React, { useCallback } from "react";
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-function UserElement(props){
-    const { user } = props;
-    console.log(user.avatarUrl)
+const CardActionsStyle = {
+  display: "flex",
+  flexDirection: "column",
+};
 
-    return (
-        <Grid item xs="auto" md={4}>
-            <Card sx={{minWidth: 50}}>
-                <CardActionArea>
-                    {user.avatarUrl &&
-                    <CardMedia
-                        component="img"
-                        alt="Avatar not found"
-                        height={60}
-                        image={user.avatarUrl}
-                    />
-                    }
-                    <CardContent>
-                        <Typography variant="body">
-                            {user.firstName} {user.lastName}<br/>
-                            <b>ID:</b> {user.id}<br/>
-                            <b>E-mail</b>: {user.email}<br/>
-                            <b>Роль</b>: {user.role}<br/>
-                            <b>Адрес</b>: {user.address}<br/>
-                            <b>Номер телефона</b>: {user.phoneNumber}
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button size="small" color="primary">
-                        Удалить пользователя
-                    </Button>
-                </CardActions>
-            </Card>
-        </Grid>
-    )
+function UserElement(props) {
+  const { user } = props;
+  const navigate = useNavigate();
+
+  const onButtonClick = useCallback(() => {
+    return navigate("/orders/" + user.id);
+  }, [navigate]);
+
+  return (
+    <Grid item xs="auto" md={4}>
+      <Card sx={{ minWidth: 50 }}>
+        <CardActionArea>
+          {user.avatarUrl && (
+            <CardMedia
+              component="img"
+              alt="Avatar not found"
+              height={60}
+              image={user.avatarUrl}
+            />
+          )}
+          <CardContent>
+            <Typography variant="body">
+              {user.firstName} {user.lastName}
+              <br />
+              <b>ID:</b> {user.id}
+              <br />
+              <b>E-mail</b>: {user.email}
+              <br />
+              <b>Роль</b>: {user.role}
+              <br />
+              <b>Адрес</b>: {user.address}
+              <br />
+              <b>Номер телефона</b>: {user.phoneNumber}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        {document.location.pathname === "/admin" && (
+          <CardActions sx={CardActionsStyle}>
+            <Button size="small" color="primary">
+              Удалить пользователя
+            </Button>
+            <Button onClick={onButtonClick}>Посмотреть заказы</Button>
+          </CardActions>
+        )}
+      </Card>
+    </Grid>
+  );
 }
 
 export default UserElement;
