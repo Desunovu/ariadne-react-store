9331cca7be517e24e8b11c22bd022616d46c9be1
Kirill Tatarinov <Desunovu@gmail.com>
2023-05-27 16:01:41 +0300

    Переместить apolloClient.js в utility
    - убрать неиспользуемый код

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import client from "./apolloClient";
+import client from "./utility/apolloClient";
 import {ApolloProvider} from "@apollo/client";
 import {BrowserRouter} from "react-router-dom";
 import {AuthProvider} from "./context/authContext";
