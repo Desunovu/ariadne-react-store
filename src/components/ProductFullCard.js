fac82fe0ad6e36fc432ef53032b8713a9ee19edc
desunovu <desunovu@gmail.com>
2023-06-03 02:55:33 +0300

    Добавить отображение отзывов

--- src/components/ProductFullCard.js
+++ src/components/ProductFullCard.js
@@ -1,17 +1,8 @@
-import {
-  Box,
-  Chip,
-  Divider,
-  ImageList,
-  ImageListItem,
-  Paper,
-  Typography,
-} from "@mui/material";
-import React, { useCallback, useState } from "react";
+import {Box, Chip, Divider, ImageList, ImageListItem, Paper, Rating, Typography,} from "@mui/material";
+import React, {useCallback, useState} from "react";
 import {AddToCartButton} from "./AddToCartButton";
 
-export default function ProductFullCard(props) {
-  const { product } = props;
+export default function ProductFullCard({product}) {
   const [selectedImageUrl, setSelectedImageUrl] = useState(
     product.images[0].url
   );
@@ -21,85 +12,104 @@ export default function ProductFullCard(props) {
   }, []);
 
   return (
-    <Paper
-      sx={{
-        padding: "15px",
-        display: "flex",
-        flexDirection: "row",
-        gap: "10px",
-      }}
-    >
-      <Box
+    <div>
+      <Paper
         sx={{
-          flex: "0",
-          width: "25%",
+          padding: "15px",
           display: "flex",
           flexDirection: "row",
-          alignItems: "flex-start",
+          gap: "10px",
         }}
       >
-        <ImageList cols={1} gap={2} sx={{ width: "25%", marginTop: "0px" }}>
-          {product.images.map((image) => (
-            <ImageListItem
-              onClick={() => onImageListItemClick(image.url)}
-              sx={{ aspectRatio: "1 / 1", width: "100%" }}
-            >
-              <img
-                src={image.url}
-                alt={image.filename}
-                style={{
-                  width: "100%",
-                  objectFit: "fill",
-                }}
-              />
-            </ImageListItem>
-          ))}
-        </ImageList>
-        <Box sx={{ marginLeft: "1px", aspectRatio: "1 / 1", width: "75%" }}>
-          <img
-            src={selectedImageUrl}
-            alt="Превью товара"
-            style={{ width: "-webkit-fill-available", objectFit: "contain" }}
-          />
+        <Box
+          sx={{
+            flex: "0",
+            width: "25%",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "flex-start",
+          }}
+        >
+          <ImageList cols={1} gap={2} sx={{width: "25%", marginTop: "0px"}}>
+            {product.images.map((image) => (
+              <ImageListItem
+                onClick={() => onImageListItemClick(image.url)}
+                sx={{aspectRatio: "1 / 1", width: "100%"}}
+              >
+                <img
+                  src={image.url}
+                  alt={image.filename}
+                  style={{
+                    width: "100%",
+                    objectFit: "fill",
+                  }}
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+          <Box sx={{marginLeft: "1px", aspectRatio: "1 / 1", width: "75%"}}>
+            <img
+              src={selectedImageUrl}
+              alt="Превью товара"
+              style={{width: "-webkit-fill-available", objectFit: "contain"}}
+            />
+          </Box>
         </Box>
-      </Box>
-      <Box
-        sx={{
-          flex: "1",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "flex-start",
-        }}
-      >
-        <Typography variant="h4" gutterBottom>
-          {product.name}
-        </Typography>
-        <Divider />
-        <Typography variant="body1" gutterBottom>
-          {product.description}
-        </Typography>
-        {product.categories.map((category) => (
-          <Chip label={category.name} />
-        ))}
-        <Typography variant="h6" color="primary">
-          {product.price} руб.
-        </Typography>
-        <Typography variant="body1" color="textSecondary">
-          Осталось: {product.amount} ед.
-        </Typography>
-        <Typography variant="body1" color="textSecondary" gutterBottom>
-          Характеристики:
-        </Typography>
-        {product.characteristics.map((item) => (
-          <Typography variant="caption" color="textSecondary">
-            {item.characteristicName}:{item.value}
-            <br />
+        <Box
+          sx={{
+            flex: "1",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+          }}
+        >
+          <Typography variant="h4" gutterBottom>
+            {product.name}
           </Typography>
+          <Divider/>
+          <Typography variant="body1" gutterBottom>
+            {product.description}
+          </Typography>
+          {product.categories.map((category) => (
+            <Chip label={category.name}/>
+          ))}
+          <Typography variant="h6" color="primary">
+            {product.price} руб.
+          </Typography>
+          <Typography variant="body1" color="textSecondary">
+            Осталось: {product.amount} ед.
+          </Typography>
+          {/* Характеристики */}
+          <Typography variant="body1" color="textSecondary" gutterBottom>
+            Характеристики:
+          </Typography>
+          {product.characteristics.map((item) => (
+            <Typography variant="caption" color="textSecondary">
+              {item.characteristicName}:{item.value}
+              <br/>
+            </Typography>
+          ))}
+        </Box>
+        {/* Добавить в корзину */}
+        <Box sx={{flex: "0"}}>
+          <AddToCartButton product={product}/>
+        </Box>
+      </Paper>
+      {/* Отзывы */}
+      <Box sx={{flex: "1", marginTop: "20px"}}>
+        <Typography variant="h5" gutterBottom>
+          Отзывы:
+        </Typography>
+        {product.reviews.map((review) => (
+          <Paper key={review.id} elevation={1} sx={{padding: "10px", marginBottom: "10px"}}>
+            {/*<Typography variant="subtitle1" gutterBottom>*/}
+            {/*  Пользователь: {review.userId}*/}
+            {/*</Typography>*/}
+            <Typography variant="body1">{review.text}</Typography>
+            <Rating name="rating" value={review.rating} precision={0.5} readOnly />
+          </Paper>
         ))}
       </Box>
-      <Box sx={{ flex: "0" }}>
-        <AddToCartButton product={product} />
-      </Box>
-    </Paper>
+    </div>
   );
 }
--- src/operations/queries/getProduct.js
+++ src/operations/queries/getProduct.js
@@ -1,39 +1,43 @@
 import {gql} from "@apollo/react-hooks";
 
 export const GET_PRODUCT = gql`
-    query GetProduct(
-        $id: Int!
-    ){
-        getProduct(
-            id: $id
-        ){
-            status
-            errors{ message }
-            product{
-                id
-                name
-                price
-                amount
-                reserved
-                description
-                categories{
-                    id
-                    name
-                }
-                images{
-                  id
-                  filename
-                  url
-                }
-                reviews{
-                    userId
-                    text
-                }
-                characteristics{
-                    characteristicName
-                    value
-                }
-            }
+  query GetProduct(
+    $id: Int!
+  ) {
+    getProduct(
+      id: $id
+    ) {
+      status
+      errors {
+        message
+      }
+      product {
+        id
+        name
+        price
+        amount
+        reserved
+        description
+        categories {
+          id
+          name
         }
+        images {
+          id
+          filename
+          url
+        }
+        reviews {
+          id
+          userId
+          rating
+          text
+        }
+        characteristics {
+          characteristicName
+          value
+        }
+      }
     }
+  }
 `
