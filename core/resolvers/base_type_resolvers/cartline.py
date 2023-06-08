57def3c73d2efbaabd354fc922bdeddb7542cf60
desunovu <desunovu@gmail.com>
2023-06-08 04:35:16 +0300

    refactoring(cartline):
    - исправлены аннотации

--- core/resolvers/base_type_resolvers/cartline.py
+++ core/resolvers/base_type_resolvers/cartline.py
@@ -4,10 +4,10 @@ from core.models import CartLine, Product
 
 
 def resolve_cartine_product(cartline_product_tuple: Tuple[CartLine, Product],
-                            _info):
+                            _info) -> Product:
     return cartline_product_tuple[1]
 
 
 def resolve_cartline_amount(cartline_product_tuple: Tuple[CartLine, Product],
-                            _info):
+                            _info) -> int:
     return cartline_product_tuple[0].amount
