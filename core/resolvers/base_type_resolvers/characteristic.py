aa2d7c8436b7dc1c06991f54daabbde5adc75dba
desunovu <desunovu@gmail.com>
2023-06-08 04:33:26 +0300

    refactoring(characteristic):
    - исправлены аннотации

--- core/resolvers/base_type_resolvers/characteristic.py
+++ core/resolvers/base_type_resolvers/characteristic.py
@@ -2,9 +2,11 @@ from core.models import Characteristic
 
 
 # Characteristic
-def resolve_characteristic_id(characteristic_obj: Characteristic, _info):
+def resolve_characteristic_id(characteristic_obj: Characteristic,
+                              _info) -> int:
     return characteristic_obj.id
 
 
-def resolve_characteristic_name(characterictic_obj: Characteristic, _info):
+def resolve_characteristic_name(characterictic_obj: Characteristic,
+                                _info) -> str:
     return characterictic_obj.name
