668ab67d43363b77b02c9b910cdfe198d2a8af2c
desunovu <desunovu@gmail.com>
2023-06-09 20:12:39 +0300

    refac/feature(resolve_product_add_to_favorites):
    - добавить логирование
    - изменить вызов query
    - проверить существует ли запись

--- core/resolvers/mutations/favorite.py
+++ core/resolvers/mutations/favorite.py
@@ -1,28 +1,44 @@
 import datetime
 
-from core import db
+from core import db, logger
 from core.extras import token_required, create_result, Errors
 from core.models import Product, FavoriteProduct
 
 
 @token_required()
 def resolve_product_add_to_favorites(_obj, info, **kwargs):
-    product = db.session.query(Product).get(kwargs.get("productId"))
+    """Добавляет товар в список избранных для текущего пользователя"""
+
+    user_id = info.context.current_user.id
+
+    # Проверка существует ли товар
+    product = Product.query.get(kwargs.get("productId"))
     if not product:
         return create_result(status=False, errors=[Errors.OBJECT_NOT_FOUND])
 
-    try:
-        db.session.add(
-            FavoriteProduct(
-                user_id=info.context.current_user.id,
+    # Поиск уже существующей записи в избранном
+    existing_favorite_product = (
+        FavoriteProduct.query
+        .filter_by(user_id=user_id,product_id=product.id)
+        .first()
+    )
+
+    if not existing_favorite_product:
+        try:
+            new_favorite_product = FavoriteProduct(
+                user_id=user_id,
                 product_id=product.id,
                 addition_date=datetime.date.today()
             )
-        )
-        db.session.commit()
-    except Exception:
-        return create_result(status=False, errors=[Errors.CANT_MANAGE_FAVORITE_PRODUCT])
+            db.session.add(new_favorite_product)
+        except Exception as e:
+            logger.error(e)
+            return create_result(
+                status=False,
+                errors=[Errors.CANT_MANAGE_FAVORITE_PRODUCT]
+            )
 
+    db.session.commit()
     return create_result(product=product)
 
 
