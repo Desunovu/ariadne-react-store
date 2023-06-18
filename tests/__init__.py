6e7573fc68eb9283f5cc94dc1e30797432ec7993
desunovu <desunovu@gmail.com>
2023-06-18 20:30:20 +0300

    feat: создавать администратора в тестах

--- tests/__init__.py
+++ tests/__init__.py
@@ -18,8 +18,9 @@ def execute_query(client, token, query):
     return result
 
 
-def create_valid_user():
+def create_test_users():
     user = User(
+        id=1,
         email="valid_email@example.com",
         password=generate_password_hash("valid_password"),
         role=Roles.CUSTOMER,
@@ -27,19 +28,36 @@ def create_valid_user():
         last_name="Васечкин",
         phone_number="asd123"
     )
+    admin = User(
+        id=2,
+        email="admin@example.com",
+        password=generate_password_hash("valid_password"),
+        role=Roles.ADMIN,
+        first_name="Админ",
+        last_name="Админов",
+    )
     db.session.add(user)
+    db.session.add(admin)
     db.session.commit()
 
 
-def get_valid_user_token():
+def get_valid_user_token(admin=False):
     """Использует резолвер типа loginUser для получения токена пользователя"""
+    if not admin:
+        email = "valid_email@example.com"
+        password = "valid_password"
+    else:
+        email = "admin@example.com"
+        password = "valid_password"
+
     token = resolve_login_user(
         _obj=None,
         _info=None,
-        email="valid_email@example.com",
-        password="valid_password"
+        email=email,
+        password=password
     )["token"]
     return token
+
 
 def delete_all_from_table(model):
     # Очистка таблицы после выполнения теста
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,14 @@
 import pytest
 
 from core.models import User
-from tests import delete_all_from_table, create_valid_user, app
+from tests import delete_all_from_table, create_test_users, app
 
 
 @pytest.fixture
 def client_with_valid_db():
     with app.app_context():
         delete_all_from_table(User)
-        create_valid_user()
+        create_test_users()
 
         with app.test_client() as client:
             yield client
