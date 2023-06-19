790af1e1723fc2810291a57e6f2fa593188f72f5
desunovu <desunovu@gmail.com>
2023-06-19 13:11:18 +0300

    docs: добавить документацию к фикстуре client_with_valid_db

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,20 @@
 import pytest
+from flask.testing import FlaskClient
 
 from core.models import User
 from tests import delete_all_from_table, create_test_users, app
 
 
 @pytest.fixture
-def client_with_valid_db():
+def client_with_valid_db() -> FlaskClient:
+    """
+    Создает клиент Flask с валидной базой данных для выполнения тестов.
+
+    Yields:
+        FlaskClient: Клиент Flask для выполнения запросов.
+    """
     with app.app_context():
+        # Создать пользователей
         delete_all_from_table(User)
         create_test_users()
 
